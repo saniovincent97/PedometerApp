b982dfca2e3a4ba978f00d4160c0df98b1966d90
Sanio Vincent <svinc26@hotmail.com>
2018-03-21 19:41:59 +1000

    -Implemented gesture in settings activity

--- app/src/main/java/com/example/svinc/running/Settings.java
+++ app/src/main/java/com/example/svinc/running/Settings.java
@@ -1,14 +1,20 @@
 package com.example.svinc.running;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements GestureDetector.OnGestureListener{
+
+    private GestureDetectorCompat detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,47 @@ public class Settings extends AppCompatActivity {
             }
         });
 
+        detector = new GestureDetectorCompat(this, this);
+
     }
 
 
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        Intent intent = new Intent(Settings.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 }
